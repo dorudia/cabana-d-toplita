0aeef56afe6320bd01028df2cc8dd7229eb73b38
Doru Diaconu <dorudia@Dorus-MacBook-Pro.local>
2025-02-25 03:49:38 +0200

    Added vercel url.

--- app/lib/auth.js
+++ app/lib/auth.js
@@ -35,25 +35,6 @@ const authConfig = {
         },
       },
       async authorize(credentials, req) {
-        // // You need to provide your own logic here that takes the credentials
-        // // submitted and returns either a object representing a user or value
-        // // that is false/null if the credentials are invalid.
-        // // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-        // // You can also use the `req` object to obtain additional parameters
-        // // (i.e., the request IP address)
-        // const res = await fetch("/your/endpoint", {
-        //   method: "POST",
-        //   body: JSON.stringify(credentials),
-        //   headers: { "Content-Type": "application/json" },
-        // });
-        // const user = await res.json();
-
-        // // If no error and we have user data, return it
-        // if (res.ok && user) {
-        //   return user;
-        // }
-        // // Return null if user data could not be retrieved
-        // return null;
         return credentials; // sau user  // to be changed
       },
     }),
@@ -69,9 +50,13 @@ const authConfig = {
   },
 };
 
+export const NEXTAUTH_URL = isDevelopment
+  ? "http://localhost:3000"
+  : `https://cabana-d-toplita.vercel.app`;
+
 export const {
   auth,
   signIn,
   signOut,
   handlers: { GET, POST },
-} = NextAuth(authConfig);
+} = NextAuth(authConfig, NEXTAUTH_URL);
