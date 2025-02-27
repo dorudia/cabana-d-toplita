d5c6927ee62b1c0957b2df17c7534e9a7a4f9f20
Doru Diaconu <dorudia@Dorus-MacBook-Pro.local>
2025-02-27 09:51:50 +0200

    Changed middleware.

--- middleware.js
+++ middleware.js
@@ -1,29 +1,61 @@
+// import { NextResponse } from "next/server";
+// import { getToken } from "next-auth/jwt";
+
+// const allowedEmails = ["dorudia@gmail.com", "elamoldovan12@gmail.com"]; // înlocuiește cu email-urile permise
+
+// export async function middleware(req) {
+//   const { pathname } = req.nextUrl;
+//   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+//   // Pentru rutele din /rezervari
+//   if (pathname.startsWith("/rezervari")) {
+//     // Dacă nu e autentificat sau emailul nu e permis, redirecționează la /login
+//     if (!token || !allowedEmails.includes(token.email)) {
+//       const url = req.nextUrl.clone();
+//       url.pathname = "/";
+//       return NextResponse.redirect(url);
+//     }
+//   }
+//   // Pentru rutele din /account: doar verificăm autentificarea
+//   else if (pathname.startsWith("/account")) {
+//     if (!token) {
+//       const url = req.nextUrl.clone();
+//       url.pathname = "/login";
+//       return NextResponse.redirect(url);
+//     }
+//   }
+//   return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: ["/account/:path*", "/rezervari/:path*"],
+// };
+
 import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
-const allowedEmails = ["dorudia@gmail.com", "elamoldovan12@gmail.com"]; // înlocuiește cu email-urile permise
+const allowedEmails = ["user1@example.com", "user2@example.com"].map((email) =>
+  email.toLowerCase()
+);
 
 export async function middleware(req) {
   const { pathname } = req.nextUrl;
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
-  // Pentru rutele din /rezervari
   if (pathname.startsWith("/rezervari")) {
-    // Dacă nu e autentificat sau emailul nu e permis, redirecționează la /login
-    if (!token || !allowedEmails.includes(token.email)) {
+    if (!token || !allowedEmails.includes(token.email.toLowerCase())) {
       const url = req.nextUrl.clone();
-      url.pathname = "/";
+      url.pathname = "/login";
       return NextResponse.redirect(url);
     }
-  }
-  // Pentru rutele din /account: doar verificăm autentificarea
-  else if (pathname.startsWith("/account")) {
+  } else if (pathname.startsWith("/account")) {
     if (!token) {
       const url = req.nextUrl.clone();
       url.pathname = "/login";
       return NextResponse.redirect(url);
     }
   }
+
   return NextResponse.next();
 }
 
