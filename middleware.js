d0760ae494a23b72f1f6d279ead15c6646e99e20
Doru Diaconu <dorudia@Dorus-MacBook-Pro.local>
2025-03-04 14:18:04 +0200

    Added log for token for test.

--- middleware.js
+++ middleware.js
@@ -9,29 +9,33 @@
 import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
-const allowedEmails = ["user1@example.com", "user2@example.com"]; // înlocuiește cu email-urile permise
+const allowedEmails = [
+  process.env.NEXTAUTH_EMAIL_1,
+  process.env.NEXTAUTH_EMAIL_2,
+]; // înlocuiește cu email-urile permise
 
 export async function middleware(req) {
   const { pathname } = req.nextUrl;
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  console.log("token", token);
 
   // Pentru rutele din /rezervari
-  // if (pathname.startsWith("/rezervari")) {
-  //   // Dacă nu e autentificat sau emailul nu e permis, redirecționează la /login
-  //   if (!token || !allowedEmails.includes(token.email)) {
-  //     const url = req.nextUrl.clone();
-  //     url.pathname = "/login";
-  //     return NextResponse.redirect(url);
-  //   }
-  // }
+  if (pathname.startsWith("/rezervari")) {
+    // Dacă nu e autentificat sau emailul nu e permis, redirecționează la /login
+    if (!token || !allowedEmails.includes(token.email)) {
+      const url = req.nextUrl.clone();
+      url.pathname = "/login";
+      return NextResponse.redirect(url);
+    }
+  }
   // Pentru rutele din /account: doar verificăm autentificarea
-  // if (pathname.startsWith("/account")) {
-  //   if (!token) {
-  //     const url = req.nextUrl.clone();
-  //     url.pathname = "/login";
-  //     return NextResponse.redirect(url);
-  //   }
-  // }
+  if (pathname.startsWith("/account")) {
+    if (!token) {
+      const url = req.nextUrl.clone();
+      url.pathname = "/login";
+      return NextResponse.redirect(url);
+    }
+  }
   return NextResponse.next();
 }
 
