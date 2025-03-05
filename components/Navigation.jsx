57443597d6417f54563f533bfe1e1884ca79dd2a
Doru Diaconu <dorudia@Dorus-MacBook-Pro.local>
2025-03-05 05:00:33 +0200

    Removed account from navbar.

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -10,6 +10,11 @@ import { useState } from "react";
 const Navigation = ({ session }) => {
   const [isOpen, setIsOpen] = useState(false);
   console.log("session form navbar 12:", session?.user);
+  if (isOpen) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "auto";
+  }
 
   let navUlClasses = isOpen
     ? "flex flex-col absolute top-[77px] left-0 w-[100%] pt-16 bg-secondary h-screen items-center justify-top space-y-4 mx-auto font-geist text-xl "
@@ -19,8 +24,13 @@ const Navigation = ({ session }) => {
   return (
     <div className="p-2 py-6 dark:bg-slate-900/80 text-slate-900-foreground border-b bg-slate-200/70 backdrop-blur-sm border-slate-400/20 fixed tracking-wider top-0 w-full z-20">
       <nav className="flex items-center justify-between max-w-7xl mx-auto font-geist text-xl ">
-        <Link href="/">Home logo</Link>
+        <Link onClick={() => setIsOpen(false)} href="/">
+          Home logo
+        </Link>
         <ul className={navUlClasses}>
+          <li className="flex md:hidden" onClick={() => setIsOpen(false)}>
+            <Link href="/">Home</Link>
+          </li>
           <li onClick={() => setIsOpen(false)}>
             <Link href="/gallery">Gallery</Link>
           </li>
@@ -30,11 +40,11 @@ const Navigation = ({ session }) => {
           <li onClick={() => setIsOpen(false)}>
             <Link href="/contact">Contact</Link>
           </li>
-          {session?.user && (
+          {/* {session?.user && (
             <li onClick={() => setIsOpen(false)}>
               <Link href="/account">Account</Link>
             </li>
-          )}
+          )} */}
           {(session?.user?.email === process.env.NEXT_PUBLIC_ADMIN_EMAIL_1 ||
             session?.user?.email === process.env.NEXT_PUBLIC_ADMIN_EMAIL_2) && (
             <li onClick={() => setIsOpen(false)}>
@@ -46,20 +56,25 @@ const Navigation = ({ session }) => {
           {session?.user ? (
             <>
               {session?.user?.image ? (
-                <Image
-                  width={30}
-                  height={30}
-                  className="w-6 h-6 rounded-full"
-                  src={session.user?.image}
-                  alt={session.user?.name}
-                />
+                <Link href="/account">
+                  <Image
+                    width={30}
+                    height={30}
+                    className="w-6 h-6 rounded-full cursor-pointer"
+                    src={session.user?.image}
+                    alt={session.user?.name}
+                    priority
+                  />
+                </Link>
               ) : (
-                <span className="text-[10px] w-6 aspect-square rounded-full text-xs grid items-center justify-center bg-primary/20 p-1">
-                  {session.user?.name
-                    .split(" ")
-                    .map((char) => char.slice(0, 1).toUpperCase())
-                    .slice(0, 2)}
-                </span>
+                <Link href="/account">
+                  <span className="text-[10px] w-6 aspect-square rounded-full text-xs grid items-center justify-center bg-primary/20 p-1">
+                    {session.user?.name
+                      .split(" ")
+                      .map((char) => char.slice(0, 1).toUpperCase())
+                      .slice(0, 2)}
+                  </span>
+                </Link>
               )}
               <SignOutButton />
             </>
